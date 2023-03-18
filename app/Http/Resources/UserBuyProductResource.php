601adc78eb5ad8f05aab5fd72afca6398b18ab86
mahmoudmohamed <mahmoudmohamed199885.com>
2023-03-18 21:48:28 +0200

    f

--- app/Http/Resources/UserBuyProductResource.php
+++ app/Http/Resources/UserBuyProductResource.php
@@ -14,7 +14,7 @@ class UserBuyProductResource extends JsonResource
             'user_name' => optional($this->user)->name,
             'phone' => optional($this->user)->phone,
             'product_name' => optional($this->product)->name,
-            'product_price' => optional($this->product)->price,
+            'product_price' => (integer)optional($this->product)->price,
             'address' => 'cairo',
             'created_at' => $this->created_at,
 
